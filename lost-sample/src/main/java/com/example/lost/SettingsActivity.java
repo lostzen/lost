8f316a239528bb6b24aaeaad970ed178f9a4eef4
Chuck Greb <charles.greb@gmail.com>
2014-07-18 14:21:31 -0400

    Enables home as up for settings screen in demo

--- lost-sample/src/main/java/com/example/lost/SettingsActivity.java
+++ lost-sample/src/main/java/com/example/lost/SettingsActivity.java
@@ -3,13 +3,25 @@ package com.example.lost;
 import android.app.Activity;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.view.MenuItem;
 
 public class SettingsActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getActionBar().setDisplayHomeAsUpEnabled(true);
         getFragmentManager().beginTransaction().replace(android.R.id.content,
                 new SettingsFragment()).commit();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public static class SettingsFragment extends PreferenceFragment {
