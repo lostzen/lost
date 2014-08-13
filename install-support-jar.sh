63da84d4f30f6eeffa5cc987fdcf4eeae9a3160e
Baldur Gudbjornsson <baldur.gudbjornsson@gmail.com>
2014-08-13 13:35:17 -0400

    adds support version variable

--- install-support-jar.sh
+++ install-support-jar.sh
@@ -4,20 +4,21 @@
 #
 # Usage:
 #   install-support-jar.sh
+version=19.0.1
 
 echo "Downloading support_r19.0.1.zip"
-wget https://dl-ssl.google.com/android/repository/support_r19.0.1.zip
-unzip support_r19.0.1.zip
-mv support support_r19.0.1
+wget https://dl-ssl.google.com/android/repository/support_r$version.zip
+unzip support_r$version.zip
+mv support support_r$version
 
-echo "Installing com.android.support:support-v4:19.0.1"
+echo "Installing com.android.support:support-v4:$version"
 mvn -q install:install-file -DgroupId=com.android.support -DartifactId=support-v4 \
-  -Dversion=19.0.1 -Dpackaging=jar -Dfile=support_r19.0.1/v4/android-support-v4.jar
+  -Dversion=$version -Dpackaging=jar -Dfile=support_r$version/v4/android-support-v4.jar
 
-echo "Deleting file support_r19.0.1.zip"
-rm support_r19.0.1.zip
+echo "Deleting file support_r$version.zip"
+rm support_r$version.zip
 
-echo "Deleting folder support_r19.0.1"
-rm -rf support_r19.0.1
+echo "Deleting folder support_r$version"
+rm -rf support_r$version
 
 echo "Done!"
