eb4d4a0b8b6a97786ab6faedd651bbbeb37b53bd
Chuck Greb <charles.greb@gmail.com>
2015-03-19 13:59:41 -0400

    Updates Android SDK dependency to latest version (5.1)

--- install-android-jar.sh
+++ install-android-jar.sh
@@ -12,14 +12,14 @@
 #
 # Adapted from https://github.com/robolectric/robolectric/blob/master/scripts/install-support-jar.sh
 
-jarLocation="$ANDROID_HOME/platforms/android-19/android.jar"
+jarLocation="$ANDROID_HOME/platforms/android-22/android.jar"
 if [ ! -f "$jarLocation" ]; then
-  echo "android-19 artifact not found!";
+  echo "android-22 artifact not found!";
   exit 1;
 fi
 
 echo "Installing android:android from $jarLocation"
 mvn -q install:install-file -DgroupId=android -DartifactId=android \
-  -Dversion=4.4.2_r3 -Dpackaging=jar -Dfile="$jarLocation"
+  -Dversion=5.1_r1 -Dpackaging=jar -Dfile="$jarLocation"
 
 echo "Done!"
