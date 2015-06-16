02438563cadbfa446a2bb77915316e5795096f90
Chuck Greb <charles.greb@gmail.com>
2015-06-16 17:30:19 -0400

    Restore unit tests
    * Upgraded Robolectric to version 3.0-rc3.
    * Rolled back Gradle to version 2.2.1 due to https://github.com/robolectric/robolectric/issues/1641.

---
+++ lost/src/test/java/com/mapzen/android/lost/internal/TestClock.java
@@ -0,0 +1,14 @@
+package com.mapzen.android.lost.internal;
+
+public class TestClock implements Clock {
+    private long currentTimeInMillis;
+
+    @Override
+    public long getCurrentTimeInMillis() {
+        return currentTimeInMillis;
+    }
+
+    public void setCurrentTimeInMillis(long currentTimeInMillis) {
+        this.currentTimeInMillis = currentTimeInMillis;
+    }
+}
