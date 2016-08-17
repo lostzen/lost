2dc694a420f2cafb0f053d250554cfe10c072445
Chuck Greb <charles.greb@gmail.com>
2016-08-17 16:58:51 -0400

    Make annotations less verbose by adding permission imports

--- lost/src/main/java/com/mapzen/android/lost/api/FusedLocationProviderApi.java
+++ lost/src/main/java/com/mapzen/android/lost/api/FusedLocationProviderApi.java
@@ -8,6 +8,9 @@ import android.support.annotation.RequiresPermission;
 
 import java.io.File;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public interface FusedLocationProviderApi {
 
   /**
@@ -16,43 +19,29 @@ public interface FusedLocationProviderApi {
    */
   @Deprecated String KEY_LOCATION_CHANGED = "com.mapzen.android.lost.LOCATION";
 
-  @RequiresPermission(anyOf = {
-      "android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"
-  })
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   Location getLastLocation();
 
   void removeLocationUpdates(LocationListener listener);
 
   void removeLocationUpdates(PendingIntent callbackIntent);
 
-  @RequiresPermission(anyOf = {
-      "android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"
-  })
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   void requestLocationUpdates(LocationRequest request, LocationListener listener, Looper looper);
 
-  @RequiresPermission(anyOf = {
-      "android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"
-  })
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   void requestLocationUpdates(LocationRequest request, LocationListener listener);
 
-  @RequiresPermission(anyOf = {
-      "android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"
-  })
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   void requestLocationUpdates(LocationRequest request, PendingIntent callbackIntent);
 
-  @RequiresPermission(anyOf = {
-      "android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"
-  })
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   void setMockLocation(Location mockLocation);
 
-  @RequiresPermission(anyOf = {
-      "android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"
-  })
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   void setMockMode(boolean isMockMode);
 
-  @RequiresPermission(anyOf = {
-      "android.permission.ACCESS_COARSE_LOCATION", "android.permission.ACCESS_FINE_LOCATION"
-  })
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   void setMockTrace(final File file);
 
   /**
