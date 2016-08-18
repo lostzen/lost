69126c6babd6822dc5a3992e8800e19666d35167
Chuck Greb <ecgreb@users.noreply.github.com>
2016-08-18 12:31:24 -0400

    Remove location permissions (#81)
    * Remove location permissions from library manifest; add to sample app
    
    * Adds `@RequiresPermission` annotations to `FusedLocationProviderApi`
    
    * Documents deprecated constant and method in FusedLocationProviderApi
    
    * Make annotations less verbose by adding permission imports
    
    * Adds `throws SecurityException` to FusionEngine methods that require runtime permissions
    
    * Remove unused import
    
    * Remove setMock* permission annotations and re-order methods

--- lost/src/main/java/com/mapzen/android/lost/api/FusedLocationProviderApi.java
+++ lost/src/main/java/com/mapzen/android/lost/api/FusedLocationProviderApi.java
@@ -3,30 +3,46 @@ package com.mapzen.android.lost.api;
 import android.app.PendingIntent;
 import android.location.Location;
 import android.os.Looper;
+import android.support.annotation.RequiresPermission;
 
 import java.io.File;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
 public interface FusedLocationProviderApi {
 
+  /**
+   * @deprecated Use {@link LocationResult#hasResult(android.content.Intent)} and
+   * {@link LocationResult#extractResult(android.content.Intent)}.
+   */
   @Deprecated String KEY_LOCATION_CHANGED = "com.mapzen.android.lost.LOCATION";
 
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
   Location getLastLocation();
+
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
+  void requestLocationUpdates(LocationRequest request, LocationListener listener);
+
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
+  void requestLocationUpdates(LocationRequest request, LocationListener listener, Looper looper);
+
+  @RequiresPermission(anyOf = {ACCESS_COARSE_LOCATION, ACCESS_FINE_LOCATION})
+  void requestLocationUpdates(LocationRequest request, PendingIntent callbackIntent);
 
   void removeLocationUpdates(LocationListener listener);
 
   void removeLocationUpdates(PendingIntent callbackIntent);
 
-  void requestLocationUpdates(LocationRequest request, LocationListener listener, Looper looper);
-
-  void requestLocationUpdates(LocationRequest request, LocationListener listener);
-
-  void requestLocationUpdates(LocationRequest request, PendingIntent callbackIntent);
+  void setMockMode(boolean isMockMode);
 
   void setMockLocation(Location mockLocation);
 
-  void setMockMode(boolean isMockMode);
-
   void setMockTrace(final File file);
 
-  boolean isProviderEnabled(String provider);
+  /**
+   * @deprecated Use {@link SettingsApi#checkLocationSettings(LostApiClient,
+   * LocationSettingsRequest)}.
+   */
+  @Deprecated boolean isProviderEnabled(String provider);
 }
