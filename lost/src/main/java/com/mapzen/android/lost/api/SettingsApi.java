fe8fc718b35479c656d5dc9e88f607e17e2fa18e
Sarah Lensing <sarah@mapzen.com>
2016-09-08 11:54:38 -0600

    SettingsApi javadoc

--- lost/src/main/java/com/mapzen/android/lost/api/SettingsApi.java
+++ lost/src/main/java/com/mapzen/android/lost/api/SettingsApi.java
@@ -2,6 +2,15 @@ package com.mapzen.android.lost.api;
 
 public interface SettingsApi {
 
+  /**
+   * Checks if the relevant system settings are enabled on the device to carry out the desired
+   * location request.
+   *
+   * @param apiClient Client which does not need to be connected at time of request.
+   * @param request Location requirements.
+   * @return a PendingResult which can be used to check status of request and optionally resolve
+   * resolutions.
+   */
   PendingResult<LocationSettingsResult> checkLocationSettings(LostApiClient apiClient,
       LocationSettingsRequest request);
 }
