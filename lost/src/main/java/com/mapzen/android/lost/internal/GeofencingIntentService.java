2a86e1b90502c40efaf1ccb9cd144ff8a134e50b
Sarah Lensing <sarah@mapzen.com>
2016-10-03 18:47:47 -0600

    Remove example toast code

--- lost/src/main/java/com/mapzen/android/lost/internal/GeofencingIntentService.java
+++ lost/src/main/java/com/mapzen/android/lost/internal/GeofencingIntentService.java
@@ -4,10 +4,6 @@ import com.mapzen.android.lost.api.GeofencingIntentSender;
 
 import android.app.IntentService;
 import android.content.Intent;
-import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.widget.Toast;
 
 /**
  * Handles receiving proximity alerts triggered by the {@link android.location.LocationManager} and
@@ -25,17 +21,6 @@ public class GeofencingIntentService extends IntentService {
   @Override protected void onHandleIntent(Intent intent) {
     GeofencingIntentSender intentGenerator = new GeofencingIntentSender(this);
     intentGenerator.sendIntent(intent);
-
-    final Bundle extras = intent.getExtras();
-    Handler handler = new Handler(Looper.getMainLooper());
-    handler.post(new Runnable() {
-      @Override public void run() {
-        for (String key : extras.keySet()) {
-          Object val = extras.get(key);
-          Toast.makeText(GeofencingIntentService.this, key + "=" + val, Toast.LENGTH_SHORT).show();
-        }
-      }
-    });
   }
 
 }
