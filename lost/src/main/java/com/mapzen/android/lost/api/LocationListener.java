a60c1cfc42345b2a8639f14d6fd7ab89e67a446c
Sarah Lensing <sarah@mapzen.com>
2016-11-08 16:27:26 -0700

    Deprecate and document provider enabled/disabled methods

--- lost/src/main/java/com/mapzen/android/lost/api/LocationListener.java
+++ lost/src/main/java/com/mapzen/android/lost/api/LocationListener.java
@@ -1,6 +1,7 @@
 package com.mapzen.android.lost.api;
 
 import android.location.Location;
+import android.os.Looper;
 
 /**
  * Used for receiving notifications from the FusedLocationProviderApi when the location has changed.
@@ -17,14 +18,28 @@ public interface LocationListener {
   void onLocationChanged(Location location);
 
   /**
-   * Called when a location provider is disabled.
+   * Called when a location provider is disabled. You will only receive updates for the priority
+   * level set in the location request used to register for updates in
+   * {@link FusedLocationProviderApi#requestLocationUpdates(LostApiClient, LocationRequest,
+   * LocationListener)}. Ie. {@link LocationRequest#PRIORITY_HIGH_ACCURACY} will invoke this method
+   * for gps and network changes but {@link LocationRequest#PRIORITY_BALANCED_POWER_ACCURACY} will
+   * only invoke it for network changes. This method will be removed in the next major release, it
+   * is recommended that you use {@link LocationAvailability} instead.
    * @param provider the disabled provider.
    */
+  @Deprecated
   void onProviderDisabled(String provider);
 
   /**
-   * Called when a location provider is enabled.
+   * Called when a location provider is enabled. You will only receive updates for the priority
+   * level set in the location request used to register for updates in
+   * {@link FusedLocationProviderApi#requestLocationUpdates(LostApiClient, LocationRequest,
+   * LocationListener)}. Ie. {@link LocationRequest#PRIORITY_HIGH_ACCURACY} will invoke this method
+   * for gps and network changes but {@link LocationRequest#PRIORITY_BALANCED_POWER_ACCURACY} will
+   * only invoke it for network changes. This method will be removed in the next major release, it
+   * is recommended that you use {@link LocationAvailability} instead.
    * @param provider the enabled provider.
    */
+  @Deprecated
   void onProviderEnabled(String provider);
 }
