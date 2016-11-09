48a050a5f4ca7df691ee6952581e6e06e1d0de7f
Sarah Lensing <sarah@mapzen.com>
2016-11-09 13:58:17 -0700

    Add LocationCallback listener to LocationAvailability example

--- lost-sample/src/main/java/com/example/lost/LocationAvailabilityActivity.java
+++ lost-sample/src/main/java/com/example/lost/LocationAvailabilityActivity.java
@@ -1,6 +1,7 @@
 package com.example.lost;
 
 import android.os.Bundle;
+import android.os.Looper;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
@@ -8,12 +9,31 @@ import android.widget.Button;
 import android.widget.Toast;
 
 import com.mapzen.android.lost.api.LocationAvailability;
+import com.mapzen.android.lost.api.LocationCallback;
+import com.mapzen.android.lost.api.LocationRequest;
+import com.mapzen.android.lost.api.LocationResult;
 import com.mapzen.android.lost.api.LocationServices;
 import com.mapzen.android.lost.api.LostApiClient;
+
+import static com.mapzen.android.lost.api.LocationRequest.PRIORITY_HIGH_ACCURACY;
 
 public class LocationAvailabilityActivity extends AppCompatActivity {
 
     LostApiClient client;
+
+    LocationCallback callback = new LocationCallback() {
+        @Override
+        public void onLocationAvailability(LocationAvailability locationAvailability) {
+            boolean isAvailable = locationAvailability.isLocationAvailable();
+            Toast.makeText(LocationAvailabilityActivity.this, isAvailable ? R.string.location_available
+                    : R.string.location_unavailable, Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onLocationResult(LocationResult result) {
+
+        }
+    };
 
     @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -56,5 +76,11 @@ public class LocationAvailabilityActivity extends AppCompatActivity {
         boolean isAvailable = availability.isLocationAvailable();
         Toast.makeText(LocationAvailabilityActivity.this, isAvailable ? R.string.location_available
                 : R.string.location_unavailable, Toast.LENGTH_SHORT).show();
+
+        final LocationRequest locationRequest = LocationRequest.create()
+                .setPriority(PRIORITY_HIGH_ACCURACY);
+        LocationServices.FusedLocationApi.requestLocationUpdates(client, locationRequest, callback,
+                Looper.myLooper());
+
     }
 }
