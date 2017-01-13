c48bfe0c193bd6d38f01d84fcdc7bb007ac86b9a
Sarah Lensing <sarah@mapzen.com>
2017-01-13 11:24:52 -0700

    Status implements Parcelable

--- lost/src/main/java/com/mapzen/android/lost/internal/DialogDisplayer.java
+++ lost/src/main/java/com/mapzen/android/lost/internal/DialogDisplayer.java
@@ -2,12 +2,13 @@ package com.mapzen.android.lost.internal;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.os.Parcelable;
 
 /**
  * Interface for displaying a dialog in an {@link android.app.Activity} for a given code and
  * {@link android.app.PendingIntent}.
  */
-public interface DialogDisplayer {
+public interface DialogDisplayer extends Parcelable {
   /**
    * Implementing class should display a dialog in the given {@link android.app.Activity} passing
    * the request and {@link android.app.PendingIntent}.
