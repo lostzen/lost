7576f01abf03a0547fd6ef6904f8c26ddc93ae09
Sarah Lensing <sarahlensing@gmail.com>
2017-05-08 17:11:21 -0600

    checkstyle

--- lost-sample/src/main/java/com/example/lost/PendingIntentActivity.java
+++ lost-sample/src/main/java/com/example/lost/PendingIntentActivity.java
@@ -17,7 +17,7 @@ import android.widget.Toast;
 public class PendingIntentActivity extends LostApiClientActivity {
 
   PendingIntent pendingIntent;
-  
+
   @Override protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_pending_intent);
