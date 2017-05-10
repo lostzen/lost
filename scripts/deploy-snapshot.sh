a2b12de251af9672549f85929ab3b52df68dba79
Sarah Lensing <sarahlensing@gmail.com>
2017-05-10 14:34:31 -0600

    update snapshot script to use printVersion task

--- scripts/deploy-snapshot.sh
+++ scripts/deploy-snapshot.sh
@@ -2,12 +2,10 @@
 #
 # Builds and uploads snapshot AARs to https://oss.sonatype.org/content/repositories/snapshots/com/mapzen/.
 #
-
-while read -r line || [[ -n "$line" ]]; do
-  if [[ $line =~ .*version=.*-SNAPSHOT.* ]]
-  then
-    ./gradlew uploadArchives -PsonatypeUsername=$SONATYPE_NEXUS_SNAPSHOTS_USERNAME  \
-        -PsonatypePassword=$SONATYPE_NEXUS_SNAPSHOTS_PASSWORD
-    break
-  fi
-done < "gradle.properties"
+version=$(./gradlew -q printVersion)
+if [[ $version =~ .*-SNAPSHOT.* ]]
+then
+  ./gradlew uploadArchives -PsonatypeUsername=$SONATYPE_NEXUS_SNAPSHOTS_USERNAME  \
+      -PsonatypePassword=$SONATYPE_NEXUS_SNAPSHOTS_PASSWORD
+  break
+fi
